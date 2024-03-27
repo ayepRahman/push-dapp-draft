225ac4870b77bbd0160c7102d2482133efc24ec0
Arif Rahman <matrep88@gmail.com>
2024-03-27 23:33:47 +0800

    added logic for filtering channels on the client side

--- src/components/Channels/Channels.tsx
+++ src/components/Channels/Channels.tsx
@@ -2,20 +2,28 @@
 
 // import Image from "next/image";
 import { useChannels } from "@/hooks/useChannels";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import InfiniteScroll from "react-infinite-scroller";
+import { Input } from "../ui/input";
 import { Skeleton } from "../ui/skeleton";
 import { ChannelRow } from "./ChannelRow";
 
 export function Channels() {
 	const { data, isLoading, isFetching, hasNextPage, fetchNextPage } =
 		useChannels();
+	const [value, setValue] = useState<string>("");
 
 	const channels = useMemo(() => {
-		return data?.pages.flatMap((page) => page.channels) ?? [];
-	}, [data]);
+		const _channels = data?.pages.flatMap((page) => page.channels) ?? [];
 
-	console.log("channels", channels);
+		if (value) {
+			return _channels.filter((channel) =>
+				channel.name.toLowerCase().includes(value.toLowerCase()),
+			);
+		}
+
+		return _channels;
+	}, [data, value]);
 
 	const renderLoader = (length: number) => (
 		<div className="flex flex-col gap-4">
@@ -30,17 +38,26 @@ export function Channels() {
 	);
 
 	return (
-		<InfiniteScroll
-			className=" flex flex-col gap-4 overflow-y-auto"
-			loadMore={() => fetchNextPage()}
-			hasMore={hasNextPage}
-			loader={renderLoader(3)}
-		>
-			{isLoading && renderLoader(10)}
-			{!!channels?.length &&
-				channels.map((channel, i) => {
-					return <ChannelRow key={`${channel.id}-${i}`} channel={channel} />;
-				})}
-		</InfiniteScroll>
+		<div>
+			<Input
+				className="mb-8"
+				placeholder="Search by name"
+				onChange={(e) => setValue(e.target.value)}
+				value={value}
+			/>
+
+			<InfiniteScroll
+				className=" flex flex-col gap-4 overflow-y-auto"
+				loadMore={() => fetchNextPage()}
+				hasMore={hasNextPage}
+				loader={renderLoader(3)}
+			>
+				{isLoading && renderLoader(10)}
+				{!!channels?.length &&
+					channels.map((channel, i) => {
+						return <ChannelRow key={`${channel.id}-${i}`} channel={channel} />;
+					})}
+			</InfiniteScroll>
+		</div>
 	);
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import { ConnectWalletButton } from "../ConnectWalletButton";
 
 export function Navbar() {
 	return (
 		<div className="mx-auto py-4 container flex items-center justify-between">
-			<div>Logo</div>
+			<Image src="/svg/push_logo.svg" height={34} width={34} alt="logo" />
 
 			<div className="flex items-center">
 				<ConnectWalletButton />
